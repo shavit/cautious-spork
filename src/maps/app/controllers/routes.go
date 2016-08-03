df89b4782c18780da67762367bd662b95a681166
shavit <shavit@users.noreply.github.com>
2016-08-03 15:12:35 -0400

    🏔

--- src/maps/app/controllers/routes.go
+++ src/maps/app/controllers/routes.go
@@ -1,11 +1,18 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+  "github.com/revel/revel"
+  "log"
+  "maps/app/models"
+)
 
 type Routes struct {
 	*revel.Controller
 }
 
 func (c Routes) Index() revel.Result {
+  var route models.Route
+  log.Println(route)
+
 	return c.RenderJson([]byte("{}"))
 }
