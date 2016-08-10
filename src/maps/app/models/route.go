38e3b07212c887d9316b843fdd167557fefc935e
shavit <shavit@users.noreply.github.com>
2016-08-10 15:12:35 -0400

    🏔

--- src/maps/app/models/route.go
+++ src/maps/app/models/route.go
@@ -12,28 +12,17 @@ type Route struct {
   Uid string `bson:"uid" json:"uid"`
   ResourceState int `bson:"resourceState" json:"resourceState"`
   Name string `bson:"name" json:"name"`
-  ActivityType string `bson:"activityType" json:"activityType"`
+  Description string `bson:"description" json:"description"`
+  Athlete string `bson:"athlete" json:"athlete"`
   Distance float64 `bson:"distance" json:"distance"`
-  AverageGrade float64 `bson:"averageGrade" json:"averageGrade"`
-  MaximumGrade float64 `bson:"maximumGrade" json:"maximumGrade"`
-  ElevationHigh float64 `bson:"elevationHigh" json:"elevationHigh"`
-  ElevationLow float64 `bson:"elevationLow" json:"elevationLow"`
-  StartPoint  []float64 `bson:"startPoint" json:"startPoint"`
-  EndPoint  []float64 `bson:"endPoint" json:"endPoint"`
-  ClimbCategory int `bson:"climbCategory" json:"climbCategory"`
-  City string `bson:"city" json:"city"`
-  State string `bson:"state" json:"state"`
-  Country string `bson:"country" json:"country"`
+  ElevationGain float64 `bson:"elevationGain" json:"elevationGain"`
+  RouteType  int `bson:"routeType" json:"routeType"`
+  SubType  int `bson:"subType" json:"subType"`
   Private bool `bson:"private" json:"private"`
   Starred bool `bson:"starred" json:"starred"`
+  Timestamp int `bson:"timestamp" json:"timestamp"`
   Created time.Time `bson:"created" json:"created"`
-  Updated time.Time `bson:"updated" json:"updated"`
-  TotalElevationGain float64 `bson:"totalElevationGain" json:"totalElevationGain"`
-  Map string `bson:"map" json:"map"`
-  EffortCount int `bson:"effortCount" json:"effortCount"`
-  AthleteCount int `bson:"athleteCount" json:"athleteCount"`
-  Hazardous bool `bson:"hazardous" json:"hazardous"`
-  StarCount int `bson:"starCount" json:"starCount"`
+  Segments []Segment `bson:"segments" json:"segments"`
 }
 
 func (r *Route) All() (routes []Route, e error){
