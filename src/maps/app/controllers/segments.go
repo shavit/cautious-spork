b73ce097324117d0cc48f407db913155b963676f
shavit <shavit@users.noreply.github.com>
2016-08-13 15:12:35 -0400

    🏔

--- src/maps/app/controllers/segments.go
+++ src/maps/app/controllers/segments.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
   "github.com/revel/revel"
+  "os"
   "log"
   "net/http"
 )
@@ -12,13 +13,23 @@ type Segments struct {
 
 func (c Segments) Explore(term string) revel.Result {
   var err error
+  var client *http.Client = &http.Client{}
   var res *http.Response
+  var req *http.Request
+  var accessToken string = os.Getenv("STRAVA_ACCESS_TOKEN")
 
-  res, err = http.Get("https://www.strava.com/api/v3/segments/explore?bounds=37.821362,-122.505373,37.842038,-122.465977")
+  req, err = http.NewRequest("GET", "https://www.strava.com/api/v3/segments/explore?bounds=37.821362,-122.505373,37.842038,-122.465977", nil)
+  if err != nil {
+    log.Fatal(err)
+  }
+  req.Header.Add("Authorization", "Bearer "+accessToken)
+
+  res, err = client.Do(req)
   if err != nil {
     log.Fatal(err)
   }
   defer res.Body.Close()
+
   log.Println(res.Body)
 
   return c.RenderJson([]byte(""))
