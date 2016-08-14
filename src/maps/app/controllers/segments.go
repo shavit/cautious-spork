02080a35e16c1b91df859ba542cbd46aa05d9966
shavit <shavit@users.noreply.github.com>
2016-08-14 15:12:35 -0400

    🏔

--- src/maps/app/controllers/segments.go
+++ src/maps/app/controllers/segments.go
@@ -3,8 +3,11 @@ package controllers
 import (
   "github.com/revel/revel"
   "os"
+  "io/ioutil"
   "log"
   "net/http"
+  "encoding/json"
+  "maps/app/models"
 )
 
 type Segments struct {
@@ -13,10 +16,10 @@ type Segments struct {
 
 func (c Segments) Explore(term string) revel.Result {
   var err error
+  var accessToken string = os.Getenv("STRAVA_ACCESS_TOKEN")
   var client *http.Client = &http.Client{}
   var res *http.Response
   var req *http.Request
-  var accessToken string = os.Getenv("STRAVA_ACCESS_TOKEN")
 
   req, err = http.NewRequest("GET", "https://www.strava.com/api/v3/segments/explore?bounds=37.821362,-122.505373,37.842038,-122.465977", nil)
   if err != nil {
@@ -30,7 +33,22 @@ func (c Segments) Explore(term string) revel.Result {
   }
   defer res.Body.Close()
 
-  log.Println(res.Body)
+  // Read the API response
+  data, err := ioutil.ReadAll(res.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
 
-  return c.RenderJson([]byte(""))
+  // Create a JSON response
+  var jsonData struct {
+    Segments []models.Segment `json: "segments"`
+  }
+  if json.Unmarshal(data, &jsonData); err != nil {
+    log.Fatal(err)
+  }
+
+  // Save the data
+  
+
+  return c.RenderJson(jsonData)
 }
--- src/maps/app/models/segment.go
+++ src/maps/app/models/segment.go
@@ -8,28 +8,29 @@ import (
 type Segment struct {
   Id bson.ObjectId `bson:"_id" json:"_id"`
   Uid string `bson:"uid" json:"uid"`
-  ResourceState int `bson:"resourceState" json:"resourceState"`
+  ResourceState int `bson:"resourceState" json:"resource_state"`
   Name string `bson:"name" json:"name"`
   ActivityType string `bson:"activityType" json:"activityType"`
   Distance float64 `bson:"distance" json:"distance"`
-  AverageGrade float64 `bson:"averageGrade" json:"averageGrade"`
-  MaximumGrade float64 `bson:"maximumGrade" json:"maximumGrade"`
-  ElevationHigh float64 `bson:"elevationHigh" json:"elevationHigh"`
-  ElevationLow float64 `bson:"elevationLow" json:"elevationLow"`
-  StartPoint  []float64 `bson:"startPoint" json:"startPoint"`
-  EndPoint  []float64 `bson:"endPoint" json:"endPoint"`
-  ClimbCategory int `bson:"climbCategory" json:"climbCategory"`
+  AverageGrade float64 `bson:"averageGrade" json:"avg_grade"`
+  MaximumGrade float64 `bson:"maximumGrade" json:"maximum_grade"`
+  ElevationHigh float64 `bson:"elevationHigh" json:"elevation_high"`
+  ElevationLow float64 `bson:"elevationLow" json:"elevation_low"`
+  StartPoint  []float64 `bson:"startPoint" json:"start_latlng"`
+  EndPoint  []float64 `bson:"endPoint" json:"end_latlng"`
+  ClimbCategory int `bson:"climbCategory" json:"climb_category"`
   City string `bson:"city" json:"city"`
   State string `bson:"state" json:"state"`
   Country string `bson:"country" json:"country"`
   Private bool `bson:"private" json:"private"`
   Starred bool `bson:"starred" json:"starred"`
-  Created time.Time `bson:"created" json:"created"`
-  Updated time.Time `bson:"updated" json:"updated"`
+  Created time.Time `bson:"created" json:"created_at"`
+  Updated time.Time `bson:"updated" json:"updated_at"`
   TotalElevationGain float64 `bson:"totalElevationGain" json:"totalElevationGain"`
   Map string `bson:"map" json:"map"`
-  EffortCount int `bson:"effortCount" json:"effortCount"`
-  AthleteCount int `bson:"athleteCount" json:"athleteCount"`
+  Points string `bson:"points" json:"points"`
+  EffortCount int `bson:"effortCount" json:"effort_count"`
+  AthleteCount int `bson:"athleteCount" json:"athlete_count"`
   Hazardous bool `bson:"hazardous" json:"hazardous"`
-  StarCount int `bson:"starCount" json:"starCount"`
+  StarCount int `bson:"starCount" json:"star_count"`
 }
