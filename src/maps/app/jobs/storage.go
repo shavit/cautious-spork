f75858cfa12527bc984439476039a221125676a8
shavit <shavit@users.noreply.github.com>
2016-08-16 15:12:35 -0400

    🏔

--- src/maps/app/jobs/storage.go
+++ src/maps/app/jobs/storage.go
@@ -1,8 +1,16 @@
 package jobs
 
+import (
+  "log"
+)
+
+type Job interface {
+  Run()
+}
+
 type StorageJob struct {
 }
 
 func (j StorageJob) Run() {
-
+  log.Println("---> Configure jobs in OnAppStart")
 }
